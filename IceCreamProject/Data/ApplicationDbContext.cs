34656fdeb25632216ede21182cd9b5ba6639aa04
Gianfranco <Gianfranco@LAPTOP-FSMPL20J>
2022-12-11 00:35:59 -0600

    Add project files.

---
+++ IceCreamProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IceCreamProject.Areas.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IceCreamProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
